346cff9655aed7768511fa11d62f4b5d3ec38bf6
Jeroen Pelgrims <git@jeroenpelgrims.com>
2025-12-21 23:41:36 +0100

    Use chat completion api

--- src/llm.rs
+++ src/llm.rs
@@ -2,12 +2,13 @@ use async_openai_compat::{
     Client,
     config::AzureConfig,
     types::{
-        ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs,
-        CreateChatCompletionResponse,
+        ChatCompletionRequestMessage, ChatCompletionRequestSystemMessage,
+        ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs,
+        CreateChatCompletionRequestArgs, CreateChatCompletionResponse,
     },
 };
 
-pub async fn send_message() -> Result<CreateChatCompletionResponse, Box<dyn std::error::Error>> {
+pub fn get_client() -> Client<AzureConfig> {
     let api_key =
         std::env::var("OPENAI_API_KEY").expect("Failed to load OPENAI_API_KEY from environment");
     let endpoint = std::env::var("MODEL_URI").expect("Failed to load MODEL_URI from environment");
@@ -16,16 +17,45 @@ pub async fn send_message() -> Result<CreateChatCompletionResponse, Box<dyn std:
         .with_api_key(api_key)
         .with_api_version("2024-12-01-preview")
         .with_deployment_id("gpt-5-mini");
-    let client = Client::with_config(config);
+    Client::with_config(config)
+}
 
-    let request = CreateChatCompletionRequestArgs::default()
-        .model("gpt-5-mini")
-        .messages([ChatCompletionRequestUserMessageArgs::default()
+// pub async fn send_message(
+//     messages: Vec<ChatCompletionRequestMessage>,
+// ) -> Result<CreateChatCompletionResponse, Box<dyn std::error::Error>> {
+//     let client = get_client();
+//     let request = CreateChatCompletionRequestArgs::default()
+//         .model("gpt-5-mini")
+//         .messages(messages)
+//         .build()?;
+
+//     let response = client.chat().create(request).await?;
+//     Ok(response)
+// }
+
+pub async fn translate(
+    input: Vec<String>,
+    target_language: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let client = get_client();
+    let messages: Vec<ChatCompletionRequestMessage> = vec![
+        ChatCompletionRequestSystemMessageArgs::default()
+            .content(
+                "You are a helpful assistant that translates text from one language to another.",
+            )
+            .build()?
+            .into(),
+        ChatCompletionRequestUserMessageArgs::default()
             .content("Your prompt here")
             .build()?
-            .into()])
+            .into(),
+    ];
+    let request = CreateChatCompletionRequestArgs::default()
+        .model("gpt-5-mini")
+        .messages(messages)
+        .store(false)
         .build()?;
-
     let response = client.chat().create(request).await?;
-    Ok(response)
+
+    Ok(())
 }
